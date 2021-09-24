cd5d2b3b96377c10836110c5886b4df0114a8ce6
okunishi <niyutadesign@gmail.com>
2021-09-24 18:55:12 +0900

    NO03 src修正

--- src/playList.js
+++ src/playList.js
@@ -10,7 +10,7 @@ export default [
     id:"518",
     title:"0518_中国の第2三国志。智勇兼備・音容兼美のイケメン皇族蘭陵王は不敗の名将",
     date:"2021.05.11",
-    audioSrc: ep0518,
+    audioSrc: 'https://storage.googleapis.com/propo-apollo.appspot.com/users/Jvmy7pwgJmbUYw0vWjNPklIQwO22/channels/FEPdlmxIDrgN6wYk0EK0/20210430_113621_533_radiohistory_ep0518_中国の第2三国志。智勇兼備・音容兼美のイケメン皇族蘭陵王は不敗の名将.mp3',
     image:"https://storage.googleapis.com/propo-apollo.appspot.com/users/X4wgS1qpQSSvTW5FSTPgO9HyS8B2/channels/FEPdlmxIDrgN6wYk0EK0/images/radireki_thumbnail_280x280.jpg",
     audioTime:"00:15:01"
   },
@@ -18,7 +18,7 @@ export default [
     id: "517",
     title: "0517_「多数決が民主主義ではない」と説いたルソーの社会契約論",
     date: "2021.05.07",
-    audioSrc: ep0517,
+    audioSrc: 'https://storage.googleapis.com/propo-apollo.appspot.com/users/Jvmy7pwgJmbUYw0vWjNPklIQwO22/channels/FEPdlmxIDrgN6wYk0EK0/20210424_105237_279_radiohistory_ep0517_「多数決が民主主義ではない」と説いたルソーの社会契約論.mp3',
     image: "https://storage.googleapis.com/propo-apollo.appspot.com/users/X4wgS1qpQSSvTW5FSTPgO9HyS8B2/channels/FEPdlmxIDrgN6wYk0EK0/images/radireki_thumbnail_280x280.jpg",
     audioTime: "00:15:52"
   },
@@ -26,7 +26,7 @@ export default [
     id: "516",
     title: "0516_「見えざる手」に隠されたアダム・スミスの経済へのアツい思い",
     date: "2021.05.04",
-    audioSrc: ep0516,
+    audioSrc: 'https://storage.googleapis.com/propo-apollo.appspot.com/users/Jvmy7pwgJmbUYw0vWjNPklIQwO22/channels/FEPdlmxIDrgN6wYk0EK0/20210424_105226_180_radiohistory_ep0516_「見えざる手」に隠されたアダム・スミスの経済へのアツい思い.mp3',
     image: "https://storage.googleapis.com/propo-apollo.appspot.com/users/X4wgS1qpQSSvTW5FSTPgO9HyS8B2/channels/FEPdlmxIDrgN6wYk0EK0/images/radireki_thumbnail_280x280.jpg",
     audioTime: "00:18:35"
   },
@@ -34,7 +34,7 @@ export default [
     id: "515",
     title: "0515_新井白石とシドッチ、鎖国日本で百年の時を経て輝いた西洋紀聞",
     date: "2021.04.30",
-    audioSrc: ep0515,
+    audioSrc: 'https://storage.googleapis.com/propo-apollo.appspot.com/users/Jvmy7pwgJmbUYw0vWjNPklIQwO22/channels/FEPdlmxIDrgN6wYk0EK0/20210416_111301_234_radiohistory_ep0515_新井白石とシドッチ、鎖国日本で百年の時を経て輝いた西洋紀聞.mp3',
     image: "https://storage.googleapis.com/propo-apollo.appspot.com/users/X4wgS1qpQSSvTW5FSTPgO9HyS8B2/channels/FEPdlmxIDrgN6wYk0EK0/images/radireki_thumbnail_280x280.jpg",
     audioTime: "00:14:17"
   },
@@ -42,7 +42,7 @@ export default [
     id: "514",
     title: "0514_「生類憐みの令」と「享保の改革」の間で政治を主導した新井白石",
     date: "2021.04.27",
-    audioSrc: ep0514,
+    audioSrc: 'https://storage.googleapis.com/propo-apollo.appspot.com/users/Jvmy7pwgJmbUYw0vWjNPklIQwO22/channels/FEPdlmxIDrgN6wYk0EK0/20210416_111247_937_radiohistory_ep0514_「生類憐みの令」と「享保の改革」の間で政治を主導した新井白石.mp3',
     image: "https://storage.googleapis.com/propo-apollo.appspot.com/users/X4wgS1qpQSSvTW5FSTPgO9HyS8B2/channels/FEPdlmxIDrgN6wYk0EK0/images/radireki_thumbnail_280x280.jpg",
     audioTime: "00:14:28"
   }
